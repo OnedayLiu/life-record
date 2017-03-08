1210bf076ace0848583d96a19fd7395b34cb93a6
OnedayLiu <599012761@qq.com>
2017-03-08 17:55:31 +0800

    chore: 增加tslint

--- FrontEnd/index.tsx
+++ FrontEnd/index.tsx
@@ -46,12 +46,12 @@
 
 
 
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 import { Hello } from './hello';
 
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    document.getElementById('example')
 );
